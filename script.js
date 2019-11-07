46770b2f872befaed7d9470ffe13e90cf8eb4e60
Alec95Hernandez <57468781+Alec95Hernandez@users.noreply.github.com>
2019-11-06 20:39:45 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+    // Function to change webpage background color
+    function changeBodyBg(color){
+        document.body.style.background = color;
+    }
+    
+    // Function to change heading background color
+    function changeHeadingBg(color){
+        document.getElementById("heading").style.background = color;
+    }
+    function changeJumboBg(color){
+      document.getElementById("jumbo")
+      .style.background = color;
+    }
